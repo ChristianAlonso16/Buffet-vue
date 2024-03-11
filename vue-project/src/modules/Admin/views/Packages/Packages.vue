0475b38d7fd275ee2ae8fd0aa85254446b741bf3
UliGarci <20203tn022@utez.edu.mx>
2024-03-11 13:16:10 -0600

    Add view packages

---
+++ vue-project/src/modules/Admin/views/Packages/Packages.vue
@@ -0,0 +1,23 @@
+<template>
+    <b-container>
+        <Cards/>
+    </b-container>
+</template>
+
+<script>
+import Cards from '../../components/packages/Cards.vue';
+export default {
+    data(){
+        return{
+            rol:false
+        }
+    },
+    components:{
+        Cards
+    }
+}
+</script>
+
+<style>
+
+</style>
